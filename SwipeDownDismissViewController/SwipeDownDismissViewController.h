36d1f5928ff5bce5faa67de1f0b9f78c4176e16e
村田真矢 <>
2019-06-22 22:00:32 +0900

    add SwipeDownDismissViewController

---
+++ SwipeDownDismissViewController/SwipeDownDismissViewController.h
@@ -0,0 +1,19 @@
+//
+//  SwipeDownDismissViewController.h
+//  SwipeDownDismissViewController
+//
+//  Created by 村田真矢 on 2019/06/22.
+//  Copyright © 2019 村田真矢. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeDownDismissViewController.
+FOUNDATION_EXPORT double SwipeDownDismissViewControllerVersionNumber;
+
+//! Project version string for SwipeDownDismissViewController.
+FOUNDATION_EXPORT const unsigned char SwipeDownDismissViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeDownDismissViewController/PublicHeader.h>
+
+
